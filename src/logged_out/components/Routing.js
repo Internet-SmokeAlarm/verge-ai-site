9bd67881c0366e25c9bd53830551bc6d64e5f2f2
Vale Tolpegin <valetolpegin@gmail.com>
2020-11-01 17:57:56 -0500

    Add empty about page

--- src/logged_out/components/Routing.js
+++ src/logged_out/components/Routing.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./home/Home";
 import Pricing from "./pricing/Pricing";
+import About from "./about/About";
 
 function Routing(props) {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/pricing" component={Pricing} />
+      <Route exact path="/about" component={About} />
     </Switch>
   );
 }
---
+++ src/logged_out/components/about/About.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+function About(props) {
+    return (
+        <Fragment>
+        </Fragment>
+    );
+}
+
+export default About;
